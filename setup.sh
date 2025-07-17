6bf50bb5c9ca0836f94ba721409d2b28453c47de
Eric Nielson <ezeike@gmail.com>
2025-07-17 06:43:29 -0400

    updates

--- setup.sh
+++ setup.sh
@@ -9,32 +9,33 @@ echo
 echo "Welcome to Canopy setup!"
 echo
 
-# Canopy branch to build, can be overridden by config.env
+CONFIG_FILE=config.env
+
+# Default canopy branch to build, can be overridden in config file
 BRANCH=beta-0.1.3
 
 # Function to show current setup configuration
 show_config() {
     echo "Loaded setup configuration:"
     echo
-    echo "SETUP_TYPE=${SETUP_TYPE}"
-    echo "DOMAIN=${DOMAIN}"
-    echo "ACME_EMAIL=${ACME_EMAIL}"
-    echo "BRANCH=$BRANCH"
+    echo "SETUP_TYPE: $SETUP_TYPE"
+    echo "DOMAIN: $DOMAIN"
+    echo "ACME_EMAIL: $ACME_EMAIL"
+    echo "BRANCH: $BRANCH"
     echo
 }
 
 # Function to save config to a file
 save_config() {
-    local config_file="config.env"
-    echo "SETUP_TYPE=${SETUP_TYPE}" > "${config_file}"
-    echo "DOMAIN=${DOMAIN}" >> "${config_file}"
-    echo "ACME_EMAIL=${ACME_EMAIL}" >> "${config_file}"
-    echo "Setup configuration saved to ${config_file}"
+    echo "SETUP_TYPE=${SETUP_TYPE}" > "${CONFIG_FILE}"
+    echo "DOMAIN=${DOMAIN}" >> "${CONFIG_FILE}"
+    echo "ACME_EMAIL=${ACME_EMAIL}" >> "${CONFIG_FILE}"
+    echo "BRANCH=${BRANCH}" >> "${CONFIG_FILE}"
+    echo "Setup configuration saved to ${CONFIG_FILE}"
 }
-
 # Function to load config from a file
 load_config() {
-    local config_file="config.env"
+    config_file="${CONFIG_FILE}"
     if [[ -f "${config_file}" ]]; then
         source "${config_file}"
     else
@@ -43,15 +44,14 @@ load_config() {
     fi
 }
 
-# Check if config.env exists and handle loading
-if [[ -f "config.env" ]]; then
+if [[ -f "$CONFIG_FILE" ]]; then
     should_load_config() {
         # Auto-load if AUTOLOAD is present
-        if grep -q "AUTOLOAD" config.env; then
+        if grep -q "AUTOLOAD=yes\|AUTOLOAD=true" $CONFIG_FILE; then
             return 0
         fi
         # Ask user for confirmation
-        read -p "config.env found. Do you want to load the existing configuration? (Y/n): " LOAD_CONFIG
+        read -p "$CONFIG_FILE found. Do you want to load the existing configuration? (Y/n): " LOAD_CONFIG
         echo
         [[ "$LOAD_CONFIG" != "n" && "$LOAD_CONFIG" != "N" ]]
     }
@@ -102,7 +102,7 @@ fi
 docker stop canopy-config > /dev/null 2>&1
 docker rm canopy-config > /dev/null 2>&1
 
-echo "setting up the validator key"
+echo "Setting up the validator key"
 docker pull canopynetwork/canopy && \
 docker run --user root -it -p 50000:50000 -p 50001:50001 -p 50002:50002 -p 50003:50003 -p 9001:9001 --name canopy-config  --volume ${PWD}/canopy_data/node1/:/root/.canopy/ canopynetwork/canopy && \
 docker stop canopy-config && docker rm canopy-config && \
